af19e92c51e68f3301d2a1d14d385bb4947905c5
medwards <a02276129@usu.edu>
2022-09-27 18:10:21 -0600

    Configured app to default to home page of blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 app_name = 'blog'
 urlpatterns = [
-    path('', views.about, name='about'),
+    path('', views.home, name='home'),
+    path('home', views.home, name="home"),
     path('about', views.about, name="about"),
     path('techtips+css', views.techtipsCss, name='techtipsCss'),
     path('techtips-css', views.techtipsNoCss, name='techtipsNoCss'),
     path('plan', views.plan, name='plan'),
-    path('home', views.home, name="home"),
     path('archive', views.archive, name="archive"),
     path('entry/<int:question_id>/', views.blogPost, name="blogPost")
 ]
